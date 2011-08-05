979dd0a99022457134545480cdcc6e83b437d0fb
WeWe Li <wewe0901@gmail.com>
2011-08-05 14:55:29 +0800

    exp environment

---
+++ Debug/weweExp1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./WeWeExpr config/exp1/optReserveEcourse.ini > output/exp1/optReserveEcourse.result
+./WeWeExpr config/exp1/optReserveUPS.ini > output/exp1/optReserveUPS.result
+./WeWeExpr config/exp1/optReserveNormal.ini > output/exp1/optReserveNormal.result
+./WeWeExpr config/exp1/optReserveUniform.ini > output/exp1/optReserveUniform.result
